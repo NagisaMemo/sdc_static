acfe45218e0caa8949c4c42e567474e57f5382ec
ssysm <me@theeditortstudio.com>
2019-02-01 23:52:21 -0500

    初始适配

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 //import particals
+import Navbar from './components/navbar/Navbar';
 
 //import pages
+import Index from './pages/index/Index';
+
 
 class App extends Component {
   render() {
     return (
-      <div>Works</div>
+      <Router>
+				<div>
+          <Navbar/>
+				  <Route exact path="/" component={Index} />
+
+				</div>
+			</Router>
     );
   }
 }
