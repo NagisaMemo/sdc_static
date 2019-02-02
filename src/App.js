3004206ee1c4a5d6a08cd84831d5eb8e8275b8e6
ssysm <me@theeditortstudio.com>
2019-02-01 21:54:51 -0500

    创建react项目

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+//import particals
+
+//import pages
+
+class App extends Component {
+  render() {
+    return (
+      <div>Works</div>
+    );
+  }
+}
+
+export default App;
